7f21835b57b41508aa6d2caf9e105c7ddedbccc1
paramrah <paramrah@students.zhaw.ch>
2026-05-14 20:32:43 +0200

    with render

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello FS2026 DevOps Course! from paramrah again XD...');
+  res.send('Hello FS2026 DevOps Course! from paramrah again XD... now with render');
 });
 
 app.listen(PORT, HOST);
